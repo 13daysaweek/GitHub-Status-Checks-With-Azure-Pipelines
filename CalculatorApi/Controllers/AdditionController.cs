5bb86974f5755025b774265bea834b97518eced1
Christopher House <chhouse@microsoft.com>
2021-05-19 10:00:14 -0500

    added addition controller

---
+++ CalculatorApi/Controllers/AdditionController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using CalculatorApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdditionController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post(MultiInputModel input)
+        {
+            IActionResult result = null;
+
+            if (input == null || input.Inputs.Count == 0)
+            {
+                result = new BadRequestResult();
+            }
+            else
+            {
+                var sum = input.Inputs.Sum(_ => _);
+                var resultModel = new ResultModel {Result = sum};
+
+                result = new OkObjectResult(resultModel);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CalculatorApi/Models/MultiInputModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CalculatorApi.Models
+{
+    public class MultiInputModel
+    {
+        public IList<decimal> Inputs { get; set; }
+    }
+}
